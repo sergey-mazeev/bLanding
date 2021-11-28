ee2104b6c79fee14b7f80626cac7d0f6e806ca60
Sergey Mazeev <sergey.mazeev@gmail.com>
2021-11-28 05:15:09 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+window.addEventListener('load', () => {
+
+    // burger
+    const burgerButton = document.getElementById('headerBurger');
+
+    if (burgerButton) {
+        const menu = document.getElementById('headerMenu');
+        burgerButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            menu.classList.toggle('header__menu_open');
+            document.body.classList.toggle('lock');
+        });
+
+        const menuClose = document.getElementById('menuClose');
+        menuClose.addEventListener('click', (e) => {
+            e.preventDefault();
+            menu.classList.toggle('header__menu_open');
+            document.body.classList.toggle('lock');
+        });
+    }
+});
 //# sourceMappingURL=main.js.map
